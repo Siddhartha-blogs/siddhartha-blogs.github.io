671b84eb304505e119cabdf4dd798fc4687e0095
Siddhartha-blogs <118605068+Siddhartha-blogs@users.noreply.github.com>
2023-07-23 23:28:07 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,76 +1,74 @@
-const sheetId = '1uen4iWMdljO1YxzwAmLsi7bdfBE9Ah_qwrBbdRtptIk';
-const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
-const sheetName = 'Sheet1';
-const query = encodeURIComponent('Select *')
-const url = `${base}&sheet=${sheetName}&tq=${query}`
-const data = []
-document.addEventListener('DOMContentLoaded', init)
-const output = document.querySelector('.output')
-function init() {
-    fetch(url)
-        .then(res => res.text())
-        .then(rep => {
-            //Remove additional text and extract only JSON:
-            const jsonData = JSON.parse(rep.substring(47).slice(0, -2));
-            console.log(rep)
-            const colz = [];
-            const tr = document.createElement('tr');
-            //Extract column labels
-            jsonData.table.cols.forEach((heading) => {
-                if (heading.label) {
-                    let column = heading.label;
-                    colz.push(column);
-                    const th = document.createElement('th');
-                    th.innerText = column;
-                    tr.appendChild(th);
-                }
-            })
-            output.appendChild(tr);
-            //extract row data:
-            jsonData.table.rows.forEach((rowData) => {
-                const row = {};
-                colz.forEach((ele, ind) => {
-                    row[ele] = (rowData.c[ind] != null) ? rowData.c[ind].v : '';
-                })
-                data.push(row);
-            })
-            processRows(data);
-        })
+// Google Sheets API Key (Replace with your actual API key)
+const GOOGLE_SHEETS_API_KEY = 'AIzaSyBDGL5m129y7EI8_urEc0mLSoxl53s_v2A';
+
+// GitHub Repository Details (Replace with your actual repository details)
+const GITHUB_REPO_OWNER = 'Siddhartha-blogs';
+const GITHUB_REPO_NAME = 'siddhartha-blogs.github.io';
+const GITHUB_ACCESS_TOKEN = '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';
+
+// Google Sheets Spreadsheet ID (Replace with your actual spreadsheet ID)
+const SPREADSHEET_ID = '1uen4iWMdljO1YxzwAmLsi7bdfBE9Ah_qwrBbdRtptIk';
+
+// Function to fetch comments from GitHub repository
+async function fetchComments() {
+  const url = `https://api.github.com/repos/${GITHUB_REPO_OWNER}/${GITHUB_REPO_NAME}/issues`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
- 
-function processRows(json) {
-    json.forEach((row) => {
-        const tr = document.createElement('tr');
-        const keys = Object.keys(row);
-    
-        keys.forEach((key) => {
-            const td = document.createElement('td');
-            td.textContent = row[key];
-            tr.appendChild(td);
-        })
-        output.appendChild(tr);
+
+// Function to display comments in the comments container
+function displayComments(comments) {
+  const commentsContainer = document.getElementById('comments-container');
+  commentsContainer.innerHTML = '';
+
+  comments.forEach(comment => {
+    const commentHTML = `
+      <div>
+        <strong>${comment.user.login}</strong> (${comment.user.email}) - ${comment.created_at}<br>
+        ${comment.body}
+      </div>
+      <hr>
+    `;
+    commentsContainer.innerHTML += commentHTML;
+  });
+}
+
+// Function to add a new comment to Google Sheets
+async function addComment(name, email, comment) {
+  const url = `https://sheets.googleapis.com/spreadsheets/d/${SPREADSHEET_ID}/values/Sheet1:append?valueInputOption=RAW`;
+  const timestamp = new Date().toLocaleString();
+  const values = [[name, email, comment, timestamp]];
+  
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${GOOGLE_SHEETS_API_KEY}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      values
     })
+  });
+
+  return response.json();
 }
 
-
-function submitComment(event) {
+// Event listener for form submission
+document.getElementById('comment-form').addEventListener('submit', async function(event) {
   event.preventDefault();
-
+  
   const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
   const comment = document.getElementById('comment').value;
 
-  gapi.client.sheets.spreadsheets.values.append({
-    spreadsheetId: sheetId,
-    range: 'Sheet1', // Replace with the sheet name and range you want to append the data to.
-    valueInputOption: 'RAW',
-    resource: {
-      values: [[name, email, comment, new Date().toLocaleString()]]
-    }
-  }).then(function(response) {
-    console.log('Comment submitted successfully!');
-    loadComments();
-  }, function(error) {
-    console.error('Error submitting comment:', error.result.error.message);
-  });
-}
+  // Add the comment to Google Sheets
+  await addComment(name, email, comment);
+  
+  // Fetch comments again to update the comments container
+  const comments = await fetchComments();
+  displayComments(comments);
+});
+
+// Initial fetch and display of comments
+fetchComments().then(displayComments);
